fe79e9da4b9e8056e20c4231cee37a14dc736e33
srashti <srashtigupta20639@gmail.com>
2021-10-10 14:24:36 +0530

    Initial commit

---
+++ src/components/Loader.js
@@ -0,0 +1,8 @@
+const Loader = ({ load }) => {
+    return (
+        (load) ?
+        <
+        div className = "loader_container" > < div className = "loaderCircle" > < /div></div > : ""
+    )
+}
+export default Loader;
